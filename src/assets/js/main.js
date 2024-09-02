24a139d79a6005320055bd38e0ae419da56ca895
Wayne Graham <wgraham@clir.org>
2024-09-02 11:20:08 -0400

    Add scroll-top button

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -36,3 +36,29 @@ var rellax = new Rellax('.rellax', {
 //   loop: true,
 //   autoplayVideos: true
 // })
+
+// Scroll Top
+// @see https://mdbootstrap.com/docs/standard/extended/back-to-top/
+let mybutton = document.getElementById('btn-back-to-top')
+
+console.log(mybutton)
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction()
+}
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = 'block'
+  } else {
+    mybutton.style.display = 'none'
+  }
+}
+
+function backToTop() {
+  document.body.scrollTop = 0
+  document.documentElement.scrollTop = 0
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener('click', backToTop)
